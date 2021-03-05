cbc7348800373b01b5758a16ffda8d3894218469
cogh <coghdevelops@gmail.com>
2021-03-05 22:25:21 +0000

    Loop Detection Algorithm
    Loop detection algorithm implemented. Activate with keypress "2". This creates fill nodes.
    
    TweenNodes are half finished. Once those are done, can begin work on generating FillCells, and TweenCells

--- Assets/Scripts/NodeDrawerScript.cs
+++ Assets/Scripts/NodeDrawerScript.cs
@@ -4,6 +4,17 @@ using UnityEditor;
 using UnityEditor.MemoryProfiler;
 using UnityEngine;
 
+class Connection
+{
+    public Connection(GameObject argFrom, GameObject argTo)
+    {
+        from = argFrom;
+        to = argTo;
+    }
+    public GameObject from;
+    public GameObject to;
+}
+
 public class NodeDrawerScript : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -22,12 +33,154 @@ public class NodeDrawerScript : MonoBehaviour
         CreateNode();
         ConnectNodes();
 
-        // Generate all cells
-        if (Input.GetKeyDown(KeyCode.C))
+        // Create tween nodes
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CreateTweenNodes();
+        }
+
+        // Create fill nodes
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // Create connection list
+            List<Connection> connections = new List<Connection>();
+
+            // Create loop list
+            List<List<GameObject>> loopList = new List<List<GameObject>>();
+
+            // Add all connections
+            foreach (GameObject node in nodeList)
+            {
+                NodeScript nodeScript = node.GetComponent<NodeScript>();
+                foreach (GameObject connectedNode in nodeScript.connectedNodes)
+                {
+                    connections.Add(new Connection(node, connectedNode));
+                }
+            }
+
+            // For each connection, search for loops
+            foreach (Connection connection in connections) // remember u gotta neglect going backwards on lonely nodes, or something, maybe do nothing if only 1 connection?
+            {
+                // Get nodes
+                GameObject fromNode = connection.from;
+                GameObject toNode   = connection.to  ;
+
+                // Left loop
+                List<GameObject> loop = new List<GameObject>();
+                GameObject firstNode   = fromNode;
+                GameObject lastNode    = fromNode;
+                GameObject currentNode = toNode  ;
+                loop.Add(firstNode);
+                int count = 0;
+                while (currentNode != firstNode && currentNode.GetComponent<NodeScript>().connectedNodes.Count > 1 && count < 1000)
+                {
+                    // Get scripts
+                    NodeScript currentScript = currentNode.GetComponent<NodeScript>();
+                    NodeScript lastScript    = lastNode   .GetComponent<NodeScript>();
+
+                    // Get last to current
+                    Vector3 lastToCurrentVector = currentNode.transform.position - lastNode.transform.position;
+
+                    // Initialise best node arbitrarily
+                    GameObject bestNextNode = currentScript.connectedNodes[0]; // needs to not include self, unbeatable?
+
+                    // If best is last, then change
+                    if (bestNextNode == lastNode) 
+                    {
+                        bestNextNode = currentScript.connectedNodes[1]; 
+                    }
+
+                    // Find correct next node (leftmost/ccw)
+                    foreach (GameObject nextNode in currentScript.connectedNodes) // NEXTNODE SHOULD NEVER BE PREVIOUS FIX THIS
+                    {
+                        // Get current to best angle - use current to last as base, and current to next/best to evaluate
+                        Vector3 currentToBestVector = bestNextNode.transform.position - currentNode.transform.position;
+                        float bestAngle = Vector3.SignedAngle(lastToCurrentVector, currentToBestVector, Vector3.up); // I understand the problem now. Signed doesnt work how I thought, has + and -
+
+                        // Get current to next angle
+                        Vector3 currentToNextVector = nextNode.transform.position - currentNode.transform.position;
+                        float nextAngle = Vector3.SignedAngle(lastToCurrentVector, currentToNextVector, Vector3.up);
+
+                        // Evaluate and replace (if better)
+                        if (nextAngle < bestAngle && nextNode != lastNode)
+                        {
+                            bestNextNode = nextNode;
+                        }
+                    }
+
+                    // Add current node to loop
+                    loop.Add(currentNode);
+
+                    // Increment node
+                    lastNode = currentNode;
+                    currentNode = bestNextNode;
+
+                    count++;
+                }
+                
+                // Add if loop is valid
+                if (currentNode == firstNode)
+                {
+                    loopList.Add(loop); // Seems to add correct loop, but also some invalids, or possibly slightly different duplicates?
+
+                    // I wonder if perhaps it is reversing, and counting the previous item as a loop?
+                }
+
+                // Don't think right loop is needed here but maybe
+            }
+
+            // Make only unique set of loops
+            List<List<GameObject>> uniqueLoopList = new List<List<GameObject>>(); // NEED TO CHECK IF THIS ALGORITHM GIVES FALSE POSITIVE ON DIFFERENT SIZED LOOPS
+            foreach (List<GameObject> loop in loopList)
+            {
+                // Find if loop exists in unique loops
+                bool loopFound = false;
+                foreach (List<GameObject> uniqueLoop in uniqueLoopList)
+                {
+                    // See if loops are identical (same nodes)
+                    bool containsSameNodes = true;
+                    foreach(GameObject node in loop)
+                    {
+                        if (!uniqueLoop.Contains(node))
+                        {
+                            containsSameNodes = false;
+                        }
+                    }
+                    // If loop is found in unique loops, has already been added (loop is found)
+                    if (containsSameNodes == true && loop.Count == uniqueLoop.Count)
+                    {
+                        loopFound = true;
+                    }
+                }
+                // Add loop if not found
+                if (!loopFound)
+                {
+                    uniqueLoopList.Add(loop);
+                }
+            }
+
+            // For every loop, generate node in the middle
+            foreach (List<GameObject> loop in uniqueLoopList)
+            {
+                Vector3 averagePosition = Vector3.zero;
+                int count = 0;
+                foreach (GameObject node in loop)
+                {
+                    averagePosition += node.transform.position;
+                    count++;
+                }
+                averagePosition /= count;
+
+                Instantiate(nodePrefab, averagePosition, Quaternion.identity);
+            }
+        }
+
+        // Generate star cells from all nodes
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             foreach (GameObject node in nodeList)
             {
-                GenerateCell(node, 1.0f);
+                GenerateStarCell(node, 1.0f);
             }
         }    
     }
@@ -42,6 +195,7 @@ public class NodeDrawerScript : MonoBehaviour
             cursor.transform.position = hit.point;
         }
     }
+
 
     void CreateNode()
     {
@@ -67,7 +221,7 @@ public class NodeDrawerScript : MonoBehaviour
                     GameObject nearestNode = GetClosestNode();
                     if (nearestNode != null && nearestNode != selection)
                     {
-                        selection.GetComponent<NodeScript>().connectedNodes.Add(nearestNode);
+                        selection  .GetComponent<NodeScript>().connectedNodes.Add(nearestNode);
                         nearestNode.GetComponent<NodeScript>().connectedNodes.Add(selection);
                     }
                     selection = null;
@@ -76,8 +230,57 @@ public class NodeDrawerScript : MonoBehaviour
                 break;
         }
     }
+    void CreateTweenNodes()
+    {
+        // Create tween nodes
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // Get all connections between nodes
+            List<Connection> nodeConnections = new List<Connection>();
+            foreach (GameObject node in nodeList)
+            {
+                NodeScript nodeScript = node.GetComponent<NodeScript>();
+                foreach (GameObject connectedNode in nodeScript.connectedNodes)
+                {
+                    // Remove connection from connectedNode to avoid duplicates
+                    NodeScript connectedNodeScript = connectedNode.GetComponent<NodeScript>();
 
-    void GenerateCell(GameObject originNode, float radius)
+                    // Create connection object
+                    Connection nodeConnection = new Connection(node, connectedNode);
+                    nodeConnections.Add(nodeConnection);
+                }
+            }
+
+            // Create tween node for each connection
+            foreach (Connection nodeConnection in nodeConnections)
+            {
+                // Get nodes
+                GameObject fromNode = nodeConnection.from;
+                GameObject toNode   = nodeConnection.to  ;
+
+                // Create node
+                Vector3 tweenPosition = Vector3.Lerp(fromNode.transform.position, toNode.transform.position, 0.5f);
+                GameObject tweenNode = Instantiate(nodePrefab, tweenPosition, Quaternion.identity);
+                nodeList.Add(tweenNode);
+
+                // Get scripts
+                NodeScript tweenScript = tweenNode.GetComponent<NodeScript>();
+                NodeScript fromScript = fromNode.GetComponent<NodeScript>();
+                NodeScript toScript = toNode.GetComponent<NodeScript>();
+
+                // Form new connections
+                tweenScript.connectedNodes.Add(fromNode);
+                tweenScript.connectedNodes.Add(toNode);
+                fromScript.connectedNodes.Add(tweenNode);
+                toScript.connectedNodes.Add(tweenNode);
+
+                // Break old connections
+                fromScript.connectedNodes.Remove(toNode);
+                toScript.connectedNodes.Remove(fromNode);
+            }
+        }
+    }
+    void GenerateStarCell(GameObject originNode, float radius)
     {
         // Original node
         NodeScript originNodeScript = originNode.GetComponent<NodeScript>();
